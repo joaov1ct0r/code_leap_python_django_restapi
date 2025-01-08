b96eeb9da9a01a2020c30efdbd84bb09c03d0466
joaov1ct0r <jov9051@outlook.com>
2025-01-07 21:28:30 -0300

    feat: added api Career model serializer

---
+++ codeleap/api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Career
+
+class CareerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Career
+        fields = ["id", "username", "created_datetime", "title", "content"]
