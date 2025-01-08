afa474ca0ceaa846382b7265b7fb7cf160cf1a7b
joaov1ct0r <jov9051@outlook.com>
2025-01-08 02:13:26 -0300

    feat: added http methods serializers

--- codeleap/api/serializers.py
+++ codeleap/api/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from .models import Career
+from .models import Post
 
-class CareerSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Career
+        model = Post
         fields = ["id", "username", "created_datetime", "title", "content"]
+
+class PostSchemaPostSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=50)
+    title = serializers.CharField(max_length=70)
+    content = serializers.CharField(max_length=100)
+
+class PostSchemaPatchSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=70)
+    content = serializers.CharField(max_length=100)
