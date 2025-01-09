32c6e2f79d8fc67a98dc6cb7c3a87f7c63da81e3
joaov1ct0r <jov9051@outlook.com>
2025-01-09 01:30:26 -0300

    feat: added id to post model

--- codeleap/api/models.py
+++ codeleap/api/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Post(models.Model):
+    objects = models.Manager()
+
+    id = models.BigAutoField(primary_key=True)
     username = models.CharField(max_length=50)
     created_datetime = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=70)
